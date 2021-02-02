21d1066032795e95f7c177f11f08926f826da386
Như Khánh <khanhnngcs17542@fpt.edu.vn>
2021-02-02 20:39:14 +0700

    Add project files.

---
+++ PracticeAPI/Models/PracticeAPIContext.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace PracticeAPI.Models
+{
+    public partial class PracticeAPIContext : DbContext
+    {
+        public PracticeAPIContext()
+        {
+        }
+
+        public PracticeAPIContext(DbContextOptions<PracticeAPIContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Product> Products { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Data Source=DESKTOP-4RBOPB6\\SQLEXPRESS;Initial Catalog=PracticeAPI;Integrated Security=True");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("Relational:Collation", "SQL_Latin1_General_CP1_CI_AS");
+
+            modelBuilder.Entity<Product>(entity =>
+            {
+                entity.ToTable("Product");
+
+                entity.Property(e => e.Description).HasMaxLength(50);
+
+                entity.Property(e => e.Name).HasMaxLength(50);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}

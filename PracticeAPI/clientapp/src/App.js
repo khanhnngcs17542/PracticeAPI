074abc7d983481aa515eca47663904250b6ae2d6
khanhnngcs17542 <54281745+khanhnngcs17542@users.noreply.github.com>
2021-02-22 15:18:31 +0700

    reactjs 1

---
+++ PracticeAPI/clientapp/src/App.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import { Route } from 'react-router';
+import Container from '@material-ui/core/Container';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import MenuTop from './components/MenuTop';
+import Home from './components/Home';
+import Product from './components/Product';
+import EditProduct from './components/EditProduct';
+export default class App extends Component {
+    displayName = App.name;
+    render() {
+        return (
+            <React.Fragment>
+                <CssBaseline />
+                <MenuTop />
+                <Container maxWidth="md">
+                    <Route exact path='/' component={Home} />
+                    <Route path='/home' component={Home} />
+                    <Route path='/products' component={Product} />
+                    <Route path='/edit/product/:id' component={EditProduct} />
+                </Container>
+            </React.Fragment>
+        );
+    }
+}
